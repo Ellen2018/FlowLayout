22c5ac75380c3d4725ed5ba2695b2f0e53eece1f
chenlang <chenlang@jztey.com>
2022-03-03 15:53:28 +0800

    演示多类型Item的适配器用法

--- app/src/main/java/com/yalemang/flowlayout/adapter/MyFlowAdapter.java
+++ app/src/main/java/com/yalemang/flowlayout/adapter/MyFlowAdapter.java
@@ -9,32 +9,52 @@ import com.yalemang.flowlayout.R;
 import com.yalemang.flowlayout.library.FlowAdapter;
 import com.yalemang.flowlayout.library.FlowViewHolder;
 
-public class MyFlowAdapter extends FlowAdapter<MyFlowAdapter.TestFlowViewHolder> {
+public class MyFlowAdapter extends FlowAdapter<FlowViewHolder> {
 
     @Override
     protected int size() {
-        return 50;
+        return 200;
     }
 
     @Override
-    protected TestFlowViewHolder createViewHolder(int itemType, int position, ViewGroup parent) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_flow,parent,false);
-        return new TestFlowViewHolder(itemView);
+    protected int itemType(int position) {
+        return position%2;
     }
 
     @Override
-    protected void bindViewHolder(TestFlowViewHolder viewHolder, int position) {
-        TestFlowViewHolder testFlowViewHolder = (TestFlowViewHolder) viewHolder;
-        testFlowViewHolder.tvFlow.setText("流式标签:"+position);
+    protected FlowViewHolder createViewHolder(int itemType, int position, ViewGroup parent) {
+        if(itemType == 0) {
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_flow_type1, parent, false);
+            return new FlowViewHolderType1(itemView);
+        }else {
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_flow_type2, parent, false);
+            return new FlowViewHolderType2(itemView);
+        }
     }
 
-    static class TestFlowViewHolder extends FlowViewHolder {
+    @Override
+    protected void bindViewHolder(FlowViewHolder viewHolder, int position) {
+          if(viewHolder instanceof FlowViewHolderType1){
+              FlowViewHolderType1 flowViewHolderType1 = (FlowViewHolderType1) viewHolder;
+              flowViewHolderType1.tvFlow.setText("标签:"+position);
+          }
+    }
+
+    static class FlowViewHolderType1 extends FlowViewHolder {
 
         TextView tvFlow;
 
-        public TestFlowViewHolder(View itemView) {
+        public FlowViewHolderType1(View itemView) {
             super(itemView);
             tvFlow = findViewById(R.id.tv_flow);
+        }
+    }
+
+    static class FlowViewHolderType2 extends FlowViewHolder {
+
+        public FlowViewHolderType2(View itemView) {
+            super(itemView);
+
         }
     }
 }
